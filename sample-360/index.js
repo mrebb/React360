8eb3047165eed98e8d6927fa280b63ad84eeef5e
Mrebb <madhu.rebbana@gmail.com>
2018-10-12 02:05:25 -0700

    React 360 playground

--- sample-360/index.js
+++ sample-360/index.js
@@ -4,9 +4,7 @@ import CurrentPost from './CurrentPost';
 import ModelView from './ModelView';
 import TopView from './TopView'
 import * as Store from './Store';
-import { API_KEY } from 'react-native-dotenv'
-
-Store.initialize(API_KEY);
+Store.initialize('key here');
 
 AppRegistry.registerComponent('TopPosts', () => TopPosts);
 AppRegistry.registerComponent('CurrentPost', () => CurrentPost);
